d864f2d0259312a2b927ffc5328731e1f68c7680
wangbin <i_wangbin@cvte.com>
2019-07-23 17:38:12 +0800

    更改文件的默认打开方式

--- send/File.cpp
+++ send/File.cpp
@@ -3,11 +3,17 @@
 #include <cstdio>
 #include <fcntl.h>
 #include <unistd.h>
-
+#include <cstring>
 #include <iostream>
 
-File::File (std::string file_path, int size) : file_path_(file_path), file_len_(size) {
-  filefd_ = ::open(file_path.c_str(), O_RDWR | O_CREAT, 0777);
+File::File (std::string file_path, int size, bool newFile) : file_path_(file_path), file_len_(size) {
+  if (newFile) {
+    filefd_ = ::open(file_path.c_str(), O_RDWR | O_CREAT, 0777);
+    lseek(filefd_, file_len_-1, SEEK_SET);
+    ::write(filefd_, "0", 1);
+  } else {
+    filefd_ = ::open(file_path_.c_str(), O_RDONLY);
+  }
   auto pos = file_path.find_last_of('/');
   file_path_ = file_path.substr(0, pos);
   file_name_ = file_path.substr(pos+1); 
@@ -29,47 +35,54 @@ File::~File() {
   filefd_ = -1;
 }
 //判断文件是否可读
-bool File::readable() {
+bool File::Readable() {
   int val = fcntl(filefd_, F_GETFL, 0);
   if (val == -1) {
     //error
   }
-  return ((val & O_ACCMODE) & (O_RDONLY | O_RDWR));
+  return ((val & O_ACCMODE) == O_RDONLY) || ((val & O_ACCMODE) == O_RDWR);
 }
 //判断文件是否可写
-bool File::writeable() {
+bool File::Writeable() {
   int val = fcntl(filefd_, F_GETFL, 0);
   if (val == -1) {
     //error
   }
-  return ((val & O_ACCMODE) & (O_WRONLY | O_RDWR));
+  return ((val & O_ACCMODE) == O_WRONLY) || ((val & O_ACCMODE) == O_RDWR);
 }
 /*
  * 根据包号计算读取的位置， 并读取固定长度的文件内容
  * 固定长度为kMaxLength
  */
-int File::read(int pack_num, char* buf) {
-  if (!readable()) {
+int File::Read(int pack_num, char* buf) {
+  if (!Readable()) {
+#if DEBUG
+    std::cout << "无法读" << std::endl;
+#endif
     throw std::string("can't read");
   } 
   lseek(filefd_, (pack_num-1)*kMaxLength, SEEK_SET); 
-  return read(filefd_, buf);
+  return ::read(filefd_, buf, kFileDataMaxLength);
 }
-void File::write(int pack_num, const char* data, int len) {
-  if (!writeable()) {
+void File::Write(int pack_num, const char* data, int len) {
+  if (!Writeable()) {
     throw std::string("can't write");
   }
   pack_is_recved_[pack_num] = true;
   lseek(filefd_, (pack_num-1)*kMaxLength, SEEK_SET);
-  write(filefd_, data, len);
+  ::write(filefd_, data, len);
 }
 
 
-std::vector<int> File::check() {
+std::vector<int> File::Check() {
   std::vector<int> res;
   for (int i = 0; i < pack_is_recved_.size(); ++i) {
     if (!pack_is_recved_[i]) res.push_back(i);
   }
   /* TODO: 检查文件完整性 <22-07-19, 王彬> */
   return res;
+}
+
+bool File::Check_at_package_number(int package_num) {
+  return pack_is_recved_[package_num];
 }
--- send/File.h
+++ send/File.h
@@ -15,23 +15,24 @@ public:
   static const int kMaxLength = kFileDataMaxLength;
   static const int kFileNameMaxLen = 100;
 public:
-  File (std::string file_path, int size=0);
+  File (std::string file_path, int size=0, bool newFile = false);
   virtual ~File ();
 
-  std::string file_name() { return file_name_; }
-  int file_len() { return file_len_; }
-  int file_max_packages() { return (file_len_+kMaxLength-1)/kFileDataMaxLength; }
+  std::string File_name() { return file_name_; }
+  int File_len() { return file_len_; }
+  int File_max_packages() { return (file_len_+kMaxLength-1)/kFileDataMaxLength; }
   //判断是否可读
   //也可用于判断文件是否可用
-  bool readable();
-  bool writeable();
+  bool Readable();
+  bool Writeable();
   //根据包号计算位置，读固定长度的内容
-  int read(int pack_num, char* buf);
+  int Read(int pack_num, char* buf);
   //根据包号计算位置，写固定长度的内容
-  void write(int pack_num, const char* data, int len);
+  void Write(int pack_num, const char* data, int len);
   //文件校验， 返回缺失的包
   //返回为空表示没有包丢失
-  std::vector<int> check();
+  std::vector<int> Check();
+  bool Check_at_package_number(int package_num);
 private:
   std::string file_path_;   //文件存在的路径
   std::string file_name_;    //文件名
