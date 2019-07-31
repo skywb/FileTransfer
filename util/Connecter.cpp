f616b0290cf8c753132b5fd09fe66c1e1dd64509
wangbin <i_wangbin@cvte.com>
2019-07-31 12:50:46 +0800

    使用分离后的Bind

--- util/Connecter.cpp
+++ util/Connecter.cpp
@@ -8,20 +8,22 @@ Connecter::Connecter (std::string group_ip, int port) {
   for (int i = 0; i < ip_vec.size(); ++i) {
     auto addr = new sockaddr_in();
     int sockfd;
-    if (true == JoinGroup(addr, &sockfd, group_ip, port, ip_vec[i])) {
-      std::cout << ip_vec[i] << " 绑定成功" << std::endl;
-      addrs_.push_back(std::make_pair(sockfd, addr));
+    if (Bind(addr, &sockfd, ip_vec[i], port)) {
+      if (true == JoinGroup(&sockfd, group_ip, ip_vec[i])) {
+        addrs_.push_back(std::make_pair(sockfd, addr));
+      } else {
+        delete  addr;
+      }
     } else {
-      delete  addr;
+      std::cout << ip_vec[i] << " 绑定失败" << std::endl;
     }
   }
   epoll_root_ = epoll_create(10);
-  int sock_cnt = addrs_.size();
-  for (int i = 0; i < sock_cnt; ++i) {
+  for (int i = 0; i < addrs_.size(); ++i) {
     event_.data.fd = addrs_[i].first;
     event_.events = EPOLLIN;
     epoll_ctl(epoll_root_, EPOLL_CTL_ADD, addrs_[i].first, &event_);
-    std::cout << inet_ntoa(addrs_[i].second->sin_addr)  << " 加入成功"<< std::endl;
+    std::cout << inet_ntoa(addrs_[i].second->sin_addr)  << "epoll 加入成功"<< std::endl;
   }
 }
 
@@ -32,9 +34,6 @@ Connecter::~Connecter() {
 }
 
 int Connecter::Recv(char* buf, int len, int timeout) {
-  //for (auto i : addrs_) {
-  //  std::cout << inet_ntoa(i.second->sin_addr) << std::endl;
-  //}
   int cnt = epoll_wait(epoll_root_, &event_, 1, timeout);
   std::cout << "epoll wait return cnt = " << cnt << std::endl;
   std::lock_guard<std::mutex> lock_guard(lock_);
@@ -45,6 +44,7 @@ int Connecter::Recv(char* buf, int len, int timeout) {
     return -1;
   }
 }
+
 int Connecter::Send(char* buf, int len) {
   std::lock_guard<std::mutex> lock_guard(lock_);
   for (auto i : addrs_) {
