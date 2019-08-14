c65a42a76a158d2e5749650a8ba4d462018c6984
wangbin <i_wangbin@cvte.com>
2019-08-14 10:13:54 +0800

    修复bug

--- recv/fileRecv.cpp
+++ recv/fileRecv.cpp
@@ -87,7 +87,7 @@ bool FileRecv(std::string group_ip, int port, std::unique_ptr<File>& file_uptr) 
         }
       }
     } else {    //没有数据， 可能已经发送完成 
-      if (time_pack_pre + std::chrono::seconds(3) <= std::chrono::system_clock::now()) {
+      if (time_pack_pre + std::chrono::seconds(20) <= std::chrono::system_clock::now()) {
         std::cout << "发送端已断开连接" << std::endl;
         break;
       }
@@ -98,11 +98,15 @@ bool FileRecv(std::string group_ip, int port, std::unique_ptr<File>& file_uptr) 
         }
       }
     }
+    while (check_package_num <= file_uptr->File_max_packages()
+        && file_uptr->Check_at_package_number(check_package_num))
+      ++check_package_num;
     if (check_package_num > file_uptr->File_max_packages()) {   //数据可能已经全部到达， 检查是否已经全部到达
       check_package_num = 0;
       while (check_package_num <= file_uptr->File_max_packages() 
           && file_uptr->Check_at_package_number(check_package_num))
         ++check_package_num;
+      std::cout << "check" << std::endl;
       if (check_package_num > file_uptr->File_max_packages()) {
         std::cout << "check end" << std::endl;
         break;
@@ -128,6 +132,7 @@ bool FileRecv(std::string group_ip, int port, std::unique_ptr<File>& file_uptr) 
   while (check_package_num <= file_uptr->File_max_packages() 
       && file_uptr->Check_at_package_number(check_package_num))
     ++check_package_num;
+  std::cout << check_package_num << " " << file_uptr->File_max_packages() << std::endl;
   return check_package_num > file_uptr->File_max_packages();
   //return true;
 }
